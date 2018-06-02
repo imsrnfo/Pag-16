d1b1a6db86fe8fc92df491ced5dbf7bc30186efd
ignaciosilvalof <imsrnfo@gmail.com>
2018-06-02 11:02:41 -0300

    Primer Commit

---
+++ src/ClaseB.java
@@ -0,0 +1,15 @@
+
+
+import javaapplication9.paquete.ClaseA;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class ClaseB {
+    
+    ClaseA claseA;
+    
+    public String perro;
+    
+}
---
+++ src/JavaApplication9.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Ignacio
+ */
+public class JavaApplication9 {
+    static ClaseB claseB;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       claseB = new ClaseB();
+       claseB.perro = "dog";
+        System.out.println(claseB.perro);
+    }
+    
+}
